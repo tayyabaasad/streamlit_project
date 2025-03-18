6144f493bb000817bc3ce455d27018d7f8542a0d
tayyabaasasd <167012406+tayyabaasad@users.noreply.github.com>
2025-03-18 19:48:56 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -23,8 +23,8 @@ st.write("Transform your files from csv and Excel formates with built-in data cl
 
 
 
-uploaded_files = st.file_uploader("upload your files(Accepts CSV and Excel):",
-                                  Type=["csv","xlsx"],accept_multiple_files=(True))
+uploaded_files = st.file_uploader("upload your files(Accepts CSV and Excel):"),
+                                  Type=["csv","xlsx"],accept_multiple_files=(True)
 
 if uploaded_files:
     for file in uploaded_files:
